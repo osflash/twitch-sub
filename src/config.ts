cceb1ac060a14e9f6229f6761cf7f876071c05c1
OsFlash <felipedasilvatomaz1996@gmail.com>
2022-05-10 09:43:48 -0300

    update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const channels = new Map<string, string>([
   ["734623227808317531", "973488436621242388"],
+  ["797288128461275136", "973498152617410630"],
 ]);
 
 export const instagramUsers = new Set<string>();
